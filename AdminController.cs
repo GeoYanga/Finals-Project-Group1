a705fc4586adadc5025fbe4897fe691d5f0509ca
Geo Yanga <147413492+GeoYanga@users.noreply.github.com>
2025-06-15 18:59:28 +0800

    Added basic Admin login support using Identity

---
+++ AdminController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace TaskTrackerWebApp.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AdminController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return Content("Admin Controller is working!");
+        }
+
+        public IActionResult Users()
+        {
+            var users = _userManager.Users.ToList();
+            return View(users);
+        }
+    }
+}
