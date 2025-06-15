843bf02a3426c57c8af8ef6ef145408dae55a2a4
Geo Yanga <147413492+GeoYanga@users.noreply.github.com>
2025-06-15 19:00:23 +0800

    admin interface update

---
+++ Users.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TaskTrackerWebApp.Views.Admin
+{
+    public class UsersModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
